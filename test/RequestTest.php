97d6e09c58ed896d6927b9c6f1fc1c52c137ab4a
mhmmdq <mhmmdq@mhmmdq.ir>
2021-10-11 11:24:07 +0330

    make test for send request

---
+++ test/RequestTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use ArashAbedii\Server\Server;
+
+
+$request = new Server;
+
+class RequestTest {
+
+    public static function test() {
+
+        $url = 'https://mhmmdq.ir/requestTest.php';
+        $params = [
+            'name' => 'Mhmmdq',
+            'email' => 'mhmmdq@mhmmdq.ir',
+            'github' => 'mhmmdq'
+        ];
+
+        $request = new Server;
+        echo $request->sendRequest($url , $params , 'post');
+
+
+    }
+}
+
+RequestTest::test();
