205b3effdd4f1492b92e23967adffeca192123e8
arash <arashabedi998@gmail.com>
2021-12-25 01:39:57 -0500

    pp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '3.x-dev',
+        'version' => '3.9999999.9999999.9999999-dev',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1d16ec9b4813185115108e763a8c93688134a122',
+        'name' => 'arashabedii/server',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arashabedii/server' => array(
+            'pretty_version' => '3.x-dev',
+            'version' => '3.9999999.9999999.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1d16ec9b4813185115108e763a8c93688134a122',
+            'dev_requirement' => false,
+        ),
+    ),
+);
