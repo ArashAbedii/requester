17e2c511361818bfdb533383834024c92be7fd73
arash <arashabedi998@gmail.com>
2022-02-03 14:03:22 +0330

    fixed autoloader

--- test.php
+++ test.php
@@ -1,15 +1,8 @@
 <?php
 
+
+
+require './vendor/autoload.php';
 use ArashAbedii\Server;
 
-require 'Server.php';
-
-$url="http://localhost:8005/app.php";
-$type="post";
-$params=['q'=>'hello','param2'=>1];
-
-$headers=['Content-Type'=>'application/json'];
-
-echo '<pre>';
-echo Server::sendRequest($url,$params,$type,$headers);
-echo '</pre>';
+echo Server::getHost();
