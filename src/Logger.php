24a79556a5126bf1f3da9ddcfd005c043c3cb257
arashabedi <arashabedi998@gmail.com>
2023-05-24 14:41:14 +0430

    renamed requester class and improved

--- src/Logger.php
+++ src/Logger.php
@@ -4,18 +4,10 @@ namespace ArashAbedii;
 
 class Logger {
 
-    public static function doLog($url,$params=[],$type='get',array $headers=[],$return_header=false,$response=null,$response_readers=null){
+    private static $log_file_path='aa_requests.log';
+
+    public static function doLog($url,$params=[],$type='get',array $headers=[],$response=null,$response_readers=null){
         
-        // $structure=[
-        //     'request_url'=>$url,
-        //     'request_method'=>strtoupper($type),
-        //     'parameters'=>$params,
-        //     'headers'=>$headers,
-        //     'return_headers_key'=>$return_header,
-        //     'response'=>$response,
-        //     'response_headers'=>$response_readers,
-        //     'request_date'=>date("Y-m-d H:i:s"),
-        // ];
 
         $http_code=!empty($response_readers->http_code) ? $response_readers->http_code : null;
 
@@ -32,7 +24,7 @@ class Logger {
         $structure.="\nrequest_date: ".date("Y-m-d H:i:s");
         $structure.="\n------------------------\n";
 
-        file_put_contents('requests.log',$structure,FILE_APPEND);
+        file_put_contents(self::$log_file_path,$structure,FILE_APPEND);
 
     }
 }
--- test.php
+++ test.php
@@ -1,8 +1,105 @@
 <?php
-
-
+use ArashAbedii\Request;
 
 require './vendor/autoload.php';
-use ArashAbedii\Server;
 
-echo Server::getHost();
+#------------------------------SIMPLE GET REQUEST---------------------------
+$url="https://reqres.in/api/users";
+$method='GET';
+$params=[
+    'delay'=>1,
+];
+$headers=[
+    'Content-Type'=>'application/json',
+];
+$logger=true; //enable or diable logging requests
+$response=Request::send($url,$params,$method,$headers,$logger);
+
+//show headers
+echo $response->headers;
+//show response body
+echo $response->body;
+
+
+
+
+#------------------------------SIMPLE POST REQUEST---------------------------
+// $url="https://reqres.in/api/users";
+// $method='POST';
+// $params=[
+//     'name'=>'myname',
+//     'job'=>'myjob'
+// ];
+// $headers=[
+//     'Content-Type'=>'application/json',
+// ];
+// $logger=false; //enable or diable logging requests
+// $response=Request::send($url,$params,$method,$headers,$logger);
+
+// //show headers
+// echo $response->headers;
+// //show response body
+// echo $response->body;
+
+
+
+
+#------------------------------SIMPLE PUT REQUEST---------------------------
+// $url="https://reqres.in/api/users/2";
+// $method='PUT';
+// $params=[
+//     'name'=>'myname2',
+//     'job'=>'myjob2'
+// ];
+// $headers=[
+//     'Content-Type'=>'application/json',
+// ];
+// $logger=false; //enable or diable logging requests
+// $response=Request::send($url,$params,$method,$headers,$logger);
+
+// //show headers
+// echo $response->headers;
+// //show response body
+// echo $response->body;
+
+
+
+#------------------------------SIMPLE PATCH REQUEST---------------------------
+// $url="https://reqres.in/api/users/2";
+// $method='PATCH';
+// $params=[
+//     'name'=>'myname2',
+//     'job'=>'myjob2'
+// ];
+// $headers=[
+//     'Content-Type'=>'application/json',
+// ];
+// $logger=true; //enable or diable logging requests
+// $response=Request::send($url,$params,$method,$headers,$logger);
+
+// //show headers
+// echo $response->headers;
+// //show response body
+// echo $response->body;
+
+
+
+#------------------------------SIMPLE DELETE REQUEST---------------------------
+// $url="https://reqres.in/api/users/2";
+// $method='DELETE';
+// $params=[
+//     //no params or your params
+// ];
+// $headers=[
+//     'Content-Type'=>'application/json',
+// ];
+// $logger=true; //enable or diable logging requests
+
+// $response=Request::send($url,$params,$method,$headers,$logger);
+
+// //show headers
+// echo $response->headers;
+// //show response body
+// echo $response->body;
+
+
